d36f92d2a238db43404b7506b0b1afe2a7ca8eba
yoshino <1151815317@qq.com>
2018-09-27 22:38:37 +0800

    add: input - input

--- packages/input/src/input.vue
+++ packages/input/src/input.vue
@@ -19,6 +19,8 @@
       <div class="el-input-group__prepend" v-if="$slots.prepend">
         <slot name="prepend"></slot>
       </div>
+      <!-- v-bind="$attrs" 把这个组件AST里的attr们 传递给input -->
+      <!-- 注意这里是:value，是单向绑定 -->
       <input
         :tabindex="tabindex"
         v-if="type !== 'textarea'"
@@ -52,6 +54,7 @@
         class="el-input__suffix"
         v-if="$slots.suffix || suffixIcon || showClear || validateState && needStatusIcon">
         <span class="el-input__suffix-inner">
+          <!-- 是否显示屁股后面那个叉叉的样式，或者显示prop里的suffixIcon -->
           <template v-if="!showClear">
             <slot name="suffix"></slot>
             <i class="el-input__icon"
@@ -64,6 +67,7 @@
             @click="clear"
           ></i>
         </span>
+        <!-- 表单验证的状态展现 -->
         <i class="el-input__icon"
           v-if="validateState"
           :class="['el-input__validateIcon', validateIcon]">
@@ -123,12 +127,14 @@
 
     data() {
       return {
+        // 初始为prop的value
         currentValue: this.value === undefined || this.value === null
           ? ''
           : this.value,
         textareaCalcStyle: {},
         hovering: false,
         focused: false,
+        // 是否处在合成状态， isOnComposition中用来给维护表单值时需要
         isOnComposition: false,
         valueBeforeComposition: null
       };
@@ -180,12 +186,14 @@
       _elFormItemSize() {
         return (this.elFormItem || {}).elFormItemSize;
       },
+      // 表格的验证状态
       validateState() {
         return this.elFormItem ? this.elFormItem.validateState : '';
       },
       needStatusIcon() {
         return this.elForm ? this.elForm.statusIcon : false;
       },
+      // 根据状态，决定不同form验证状态展现的样式
       validateIcon() {
         return {
           validating: 'el-icon-loading',
@@ -203,6 +211,7 @@
         return this.disabled || (this.elForm || {}).disabled;
       },
       showClear() {
+        // 可清除 && 非禁用 && 非只读 && 有值 && hover或focus状态
         return this.clearable &&
           !this.disabled &&
           !this.readonly &&
@@ -243,6 +252,7 @@
         }
       },
       select() {
+        // select() 方法用于选取文本域中的内容。
         (this.$refs.input || this.$refs.textarea).select();
       },
       resizeTextarea() {
@@ -264,16 +274,23 @@
         this.focused = true;
         this.$emit('focus', event);
       },
+      // 输入一段文字时的处理函数，可以通过event.type判断是on 还是update或者end
       handleComposition(event) {
+        // 结束时 关闭合成状态,将currentValue赋值为旧值
         if (event.type === 'compositionend') {
           this.isOnComposition = false;
+          // 先把currentValue还原成旧值，清空旧值
           this.currentValue = this.valueBeforeComposition;
           this.valueBeforeComposition = null;
+          // 给currentValue赋新值
           this.handleInput(event);
         } else {
           const text = event.target.value;
+          // 取得最后一个字符
           const lastCharacter = text[text.length - 1] || '';
+          // 是韩语的话，就关闭合成状态，否则打开
           this.isOnComposition = !isKorean(lastCharacter);
+          // 用valueBeforeComposition 存储合成之前的旧值
           if (this.isOnComposition && event.type === 'compositionstart') {
             this.valueBeforeComposition = text;
           }
@@ -281,7 +298,9 @@
       },
       handleInput(event) {
         const value = event.target.value;
+        // 赋值
         this.setCurrentValue(value);
+        // 合成状态下 不触发input自定义事件
         if (this.isOnComposition) return;
         this.$emit('input', value);
       },
@@ -289,8 +308,10 @@
         this.$emit('change', event.target.value);
       },
       setCurrentValue(value) {
+        // 合成状态 && 新旧值相等 return， 算是一层保险
         if (this.isOnComposition && value === this.valueBeforeComposition) return;
         this.currentValue = value;
+        // 新旧值不等的情况下更新值
         if (this.isOnComposition) return;
         this.$nextTick(this.resizeTextarea);
         if (this.validateEvent && this.currentValue === this.value) {
@@ -314,12 +335,14 @@
         };
 
         const pendant = pendantMap[place];
+        // 给前缀和后缀元素（插槽或传下来的icon样式）定位到最左边或者最右边
         if (this.$slots[pendant]) {
           el.style.transform = `translateX(${place === 'suffix' ? '-' : ''}${this.$el.querySelector(`.el-input-group__${pendant}`).offsetWidth}px)`;
         } else {
           el.removeAttribute('style');
         }
       },
+      // 更新prefix和suffix的位置 会在渲染完成或者组件更新的时候执行
       updateIconOffset() {
         this.calcIconOffset('prefix');
         this.calcIconOffset('suffix');
@@ -334,6 +357,7 @@
     },
 
     created() {
+      // 派发inputSelect事件，传递组件的初始值
       this.$on('inputSelect', this.select);
     },
 
--- src/utils/shared.js
+++ src/utils/shared.js
@@ -1,6 +1,8 @@
+// 是否定义过
 export function isDef(val) {
   return val !== undefined && val !== null;
 }
+// 判断是否是韩语
 export function isKorean(text) {
   const reg = /([(\uAC00-\uD7AF)|(\u3130-\u318F)])+/gi;
   return reg.test(text);
